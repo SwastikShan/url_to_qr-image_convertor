20649d091ca95716238d8a9338872087f3a04c83
SwastikShan <kumarshantanu4403@gmail.com>
2023-06-03 15:18:30 +0530

    qr-image generator

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from 'inquirer';
+import qr from "qr-image"
+import fs from "fs"
+inquirer
+.prompt([
+    {
+        message:"Type in your url",
+        name:"url",
+    },
+  ])
+
+  .then((answers) => {
+    const url= answers.url;
+    
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("URL.txt",url,(err)=>{
+        if(err) throw err;
+        console.log("the file has been saved");
+  });
+    })
+ 
+
+    .catch((error) => {
+    if (error.isTtyError) {
+    console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+      console.log("error");
+    }
+  });
+ 
